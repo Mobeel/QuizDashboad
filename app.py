9d6c7281fc7083f3b850f3d19a82d1040b9d6678
Mobeel <57725535+Mobeel@users.noreply.github.com>
2026-01-21 02:05:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ df = load_data()
 
 # 3. Interaction Sidebar
 st.sidebar.header("Interactive Filters")
-selected_year = st.sidebar.slider("Select Year", 1990, 2018,2020)
+selected_year = st.sidebar.slider("Select Year", 1990, 2018)
 
 filtered_df = df[df['year'] == selected_year].dropna(subset=['Access to electricity (% of population)', 'GDP per capita (current US$)'])
 
@@ -27,29 +27,30 @@ col3, col4 = st.columns(2)
 # Humne height=300 rakha hai taake 4 graphs screen par fit aayein
 with col1:
     fig1 = px.choropleth(filtered_df, locations="Country Code", color="Access to electricity (% of population)",
-                         color_continuous_scale="Blues", height=200)
+                         color_continuous_scale="Blues", height=250)
     fig1.update_layout(margin=dict(l=0, r=0, t=30, b=0)) # Margin kam kiya
     st.plotly_chart(fig1, use_container_width=True)
 
 with col2:
     trend_df = df.groupby('year')['Access to electricity (% of population)'].mean().reset_index()
     fig2 = px.line(trend_df, x='year', y='Access to electricity (% of population)', 
-                   color_discrete_sequence=['darkorange'], height=200)
+                   color_discrete_sequence=['darkorange'], height=250)
     fig2.update_layout(margin=dict(l=0, r=0, t=30, b=0))
     st.plotly_chart(fig2, use_container_width=True)
 
 with col3:
     fig3 = px.scatter(filtered_df, x="GDP per capita (current US$)", y="Access to electricity (% of population)",
-                      log_x=True, color_discrete_sequence=['steelblue'], height=200)
+                      log_x=True, color_discrete_sequence=['steelblue'], height=250)
     fig3.update_layout(margin=dict(l=0, r=0, t=30, b=0))
     st.plotly_chart(fig3, use_container_width=True)
 
 with col4:
     counts = filtered_df[['Access to electricity (% of population)', 'GDP per capita (current US$)']].count()
-    fig4 = px.bar(x=counts.index, y=counts.values, color_discrete_sequence=['#9ECAE1'], height=200)
+    fig4 = px.bar(x=counts.index, y=counts.values, color_discrete_sequence=['#9ECAE1'], height=250)
     fig4.update_layout(margin=dict(l=0, r=0, t=30, b=0))
 
     st.plotly_chart(fig4, use_container_width=True)
+
 
 
 
